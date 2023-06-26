d5a72bf964bd660c6df5e3e197c0c9d2e2677870
Thanh Nguyen <thanhn@microsoft.com>
2023-06-26 23:08:05 +1000

    cleanup

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=hainam',
-    'username' => 'hainam',
-    'password' => 'h@in@mp@$$',
-    'charset' => 'utf8',
-];
